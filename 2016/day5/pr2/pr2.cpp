05e675952a6af141deac4290e1baa679d4e9d590
cescgina <cescgina@gmail.com>
2020-08-08 15:05:52 +0200

    Solved day 5

---
+++ 2016/day5/pr2/pr2.cpp
@@ -0,0 +1,53 @@
+#include <set>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <utility>
+#include <unistd.h>
+#include <openssl/md5.h>
+
+bool checkZeros(char* answer){
+    for (int i=0; i<5; i++){
+        if (answer[i] != '0') return false;
+    }    
+    return true;
+}
+
+int main(int argc, char** argv){
+    static const char hexDigits[17] = "0123456789abcdef";
+    unsigned char digest[MD5_DIGEST_LENGTH];
+    char digest_str[2*MD5_DIGEST_LENGTH+1];
+    std::string file_name, secret_key, complete_key;
+    getopt(argc, argv, "");
+    file_name = std::string(argv[optind]);
+    std::ifstream input_file;
+    input_file.open(file_name);
+    std::getline(input_file, secret_key);
+    input_file.close();
+    int chars = 0, suffix = 0, ind = 0;
+    std::vector<std::string> pass = {".", ".", ".", ".", ".", ".", ".", "."};
+    while (chars < 8){
+        complete_key = secret_key + std::to_string(suffix); 
+        // Count digest
+        MD5( (const unsigned char*)complete_key.c_str(), complete_key.length(), digest );
+        // Convert the hash into a hex string form
+        for(int i = 0; i < MD5_DIGEST_LENGTH; i++ )
+        {
+            digest_str[i*2]   = hexDigits[(digest[i] >> 4) & 0xF];
+            digest_str[i*2+1] = hexDigits[digest[i] & 0xF];
+        }
+        digest_str[MD5_DIGEST_LENGTH*2] = '\0';
+        if (checkZeros(digest_str)) {
+            ind = digest_str[5]-'0';
+            if (ind < 8 and pass[ind] == "."){
+                pass[ind] = digest_str[6];
+                chars++;
+            }
+        }
+        suffix++;
+    }
+    for (auto it=pass.begin(); it != pass.end(); it++){
+        std::cout << *it;
+    }
+    std::cout << std::endl;;
+}
