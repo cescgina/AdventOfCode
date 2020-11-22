4f52138ca20af493428e0bb075671a8b510ad44a
cescgina <cescgina@gmail.com>
2020-11-22 15:36:48 +0100

    Solved day21

--- 2018/day19/pr2/pr2.cpp
+++ 2018/day19/pr2/pr2.cpp
@@ -4,7 +4,7 @@
 
 int main(){
     int r0 = 0, r1 = 1, r4=10551275;
-    while (r1 < sqrt(r4)+1){
+    while (r1 < sqrt(r4)){
         if (r4%r1 == 0){
             r0 += r1;
             r0 += r4/r1;
