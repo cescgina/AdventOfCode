0a825f31a9f665a738c43d4f0a94718a19e44595
cescgina <cescgina@gmail.com>
2020-12-22 23:17:18 +0100

    Solved day9

---
+++ 2019/day9/pr1/pr1.cpp
@@ -0,0 +1,305 @@
+#include <queue>
+#include <vector>
+#include <fstream>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unistd.h>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
+
+typedef std::vector<long long> registers;
+typedef std::vector<std::string> to_parse;
+typedef std::unordered_map<unsigned long long, long long> program;
+bool DEBUG = false;
+
+void execute_program(program& instructions, std::queue<long long>& output, std::queue<long long>& inputs){
+    long long i = 0;
+    long long n = instructions.size();
+    long long relative_base = 0;
+    while (i < n){
+        long long ins = instructions[i];
+        int mode_a, mode_b, mode_c;
+        int opcode = ins % 100;
+        mode_c = ins / 10000;
+        ins %= 10000;
+        mode_b = ins / 1000;
+        ins %= 1000;
+        mode_a = ins / 100;
+        long long operand_a, operand_b, operand_c;
+        if (DEBUG){
+            std::cout << i << " " << instructions[i] << " " << opcode << " " << mode_a << " " << mode_b << " " << mode_c << ":";
+        }
+        if (opcode == 99){
+            // halt
+            if (DEBUG){
+                std::cout << std::endl;
+            }
+            break;
+        }
+        else if (opcode == 1){
+            // add
+            if (mode_a == 1){
+                operand_a = instructions[i+1];
+            }
+            else if (mode_a == 2){
+                operand_a = instructions[instructions[i+1]+relative_base];
+            }
+            else{
+                operand_a = instructions[instructions[i+1]];
+            }
+            if (mode_b == 1){
+                operand_b = instructions[i+2];
+            }
+            else if (mode_b == 2){
+                operand_b = instructions[instructions[i+2]+relative_base];
+            }
+            else{
+                operand_b = instructions[instructions[i+2]];
+            }
+            if (mode_c == 2){
+                operand_c = instructions[i+3]+relative_base;
+            }
+            else{
+                operand_c = instructions[i+3];
+            }
+            instructions[operand_c] = operand_a+operand_b;
+            if (DEBUG){
+                std::cout << " " << operand_a << " " << operand_b << " " << operand_c << " " << instructions[operand_c] << std::endl;
+            }
+            i += 4;
+        }
+        else if (opcode == 2){
+            // mul
+            if (mode_a == 1){
+                operand_a = instructions[i+1];
+            }
+            else if (mode_a == 2){
+                operand_a = instructions[instructions[i+1]+relative_base];
+            }
+            else{
+                operand_a = instructions[instructions[i+1]];
+            }
+            if (mode_b == 1){
+                operand_b = instructions[i+2];
+            }
+            else if (mode_b == 2){
+                operand_b = instructions[instructions[i+2]+relative_base];
+            }
+            else{
+                operand_b = instructions[instructions[i+2]];
+            }
+            if (mode_c == 2){
+                operand_c = instructions[i+3]+relative_base;
+            }
+            else{
+                operand_c = instructions[i+3];
+            }
+            instructions[operand_c] = operand_a*operand_b;
+            if (DEBUG){
+                std::cout << " " << operand_a << " " << operand_b << " " << operand_c << " " << instructions[operand_c] << std::endl;
+            }
+            i += 4;
+        }
+        else if (opcode == 3){
+            // store
+            if (mode_a == 2){
+                operand_a = instructions[i+1]+relative_base;
+            }
+            else{
+                operand_a = instructions[i+1];
+            }
+            long long input = inputs.front();
+            inputs.pop();
+            instructions[operand_a] = input;
+            if (DEBUG){
+                std::cout << " " << operand_a << " " << instructions[operand_a] << std::endl;
+            }
+            i += 2;
+        }
+        else if (opcode == 4){
+            // load
+            if (mode_a == 1){
+                operand_a = instructions[i+1];
+            }
+            else if (mode_a == 2){
+                operand_a = instructions[instructions[i+1]+relative_base];
+            }
+            else{
+                operand_a = instructions[instructions[i+1]];
+            }
+            output.push(operand_a);
+            if (DEBUG){
+                std::cout << " " << operand_a << " " << i+1 << " " << instructions[i+1] << std::endl;
+            }
+            std::cout << operand_a << std::endl;
+            i += 2;
+        }
+        else if (opcode == 5){
+            // jump-if-not-zero
+            if (mode_a == 1){
+                operand_a = instructions[i+1];
+            }
+            else if (mode_a == 2){
+                operand_a = instructions[instructions[i+1]+relative_base];
+            }
+            else{
+                operand_a = instructions[instructions[i+1]];
+            }
+            if (mode_b == 1){
+                operand_b = instructions[i+2];
+            }
+            else if (mode_b == 2){
+                operand_b = instructions[instructions[i+2]+relative_base];
+            }
+            else{
+                operand_b = instructions[instructions[i+2]];
+            }
+            if (operand_a != 0) {
+                i = operand_b;
+            }
+            else{
+                i += 3;
+            }
+            if (DEBUG){
+                std::cout << " " << operand_a << " " << operand_b << std::endl;
+            }
+        }
+        else if (opcode == 6){
+            // jump-if-zero
+            if (mode_a == 1){
+                operand_a = instructions[i+1];
+            }
+            else if (mode_a == 2){
+                operand_a = instructions[instructions[i+1]+relative_base];
+            }
+            else{
+                operand_a = instructions[instructions[i+1]];
+            }
+            if (mode_b == 1){
+                operand_b = instructions[i+2];
+            }
+            else if (mode_b == 2){
+                operand_b = instructions[instructions[i+2]+relative_base];
+            }
+            else{
+                operand_b = instructions[instructions[i+2]];
+            }
+            if (operand_a == 0) {
+                i = operand_b;
+            }
+            else{
+                i += 3;
+            }
+            if (DEBUG){
+                std::cout << " " << operand_a << " " << operand_b << std::endl;
+            }
+        }
+        else if (opcode == 7){
+            // less-than
+            if (mode_a == 1){
+                operand_a = instructions[i+1];
+            }
+            else if (mode_a == 2){
+                operand_a = instructions[instructions[i+1]+relative_base];
+            }
+            else{
+                operand_a = instructions[instructions[i+1]];
+            }
+            if (mode_b == 1){
+                operand_b = instructions[i+2];
+            }
+            else if (mode_b == 2){
+                operand_b = instructions[instructions[i+2]+relative_base];
+            }
+            else{
+                operand_b = instructions[instructions[i+2]];
+            }
+            if (mode_c == 2){
+                operand_c = instructions[i+3]+relative_base;
+            }
+            else{
+                operand_c = instructions[i+3];
+            }
+            instructions[operand_c] = operand_a<operand_b;
+            if (DEBUG){
+                std::cout << " " << operand_a << " " << operand_b << " " << operand_c << " " << instructions[operand_c] << std::endl;
+            }
+            i += 4;
+        }
+        else if (opcode == 8){
+            // equals
+            if (mode_a == 1){
+                operand_a = instructions[i+1];
+            }
+            else if (mode_a == 2){
+                operand_a = instructions[instructions[i+1]+relative_base];
+            }
+            else{
+                operand_a = instructions[instructions[i+1]];
+            }
+            if (mode_b == 1){
+                operand_b = instructions[i+2];
+            }
+            else if (mode_b == 2){
+                operand_b = instructions[instructions[i+2]+relative_base];
+            }
+            else{
+                operand_b = instructions[instructions[i+2]];
+            }
+            if (mode_c == 2){
+                operand_c = instructions[i+3]+relative_base;
+            }
+            else{
+                operand_c = instructions[i+3];
+            }
+            instructions[operand_c] = operand_a==operand_b;
+            if (DEBUG){
+                std::cout << " " << operand_a << " " << operand_b << " " << operand_c << " " << instructions[operand_c] << std::endl;
+            }
+            i += 4;
+        }
+        else if (opcode == 9){
+            // adjust relative base
+            if (mode_a == 1){
+                operand_a = instructions[i+1];
+            }
+            else if (mode_a == 2){
+                operand_a = instructions[instructions[i+1]+relative_base];
+            }
+            else{
+                operand_a = instructions[instructions[i+1]];
+            }
+            relative_base += operand_a;
+            if (DEBUG){
+                std::cout << " " << operand_a << " " << relative_base << std::endl;
+            }
+            i += 2;
+        }
+        else{
+            std::cout << "Something wierd found " << i << " " << instructions[i] << " " << opcode << std::endl;
+        }
+    }
+
+}
+int main(int argc, char** argv){
+    std::string file_name;
+    getopt(argc, argv, "");
+    file_name = std::string(argv[optind]);
+    std::ifstream input_file;
+    input_file.open(file_name);
+    std::string line; 
+    std::getline(input_file, line);
+    to_parse before_reg;
+    registers instructions;
+    program code;
+    boost::split(before_reg, line, boost::is_any_of(", "), boost::token_compress_on);
+    std::transform(before_reg.begin(), before_reg.end(), std::back_inserter(instructions), [](std::string& c) -> long long { return std::stoll(c);});
+    for (size_t i = 0; i<instructions.size(); i++){
+        code[i] = instructions[i];
+    }
+    std::queue<long long> inputs({1});
+    std::queue<long long> output;
+    execute_program(code, output, inputs);
+}
